39de8a28ef53d0294765755c9294e75317c5293f
AHMED <hossainahmad76@gmail.com>
2023-12-30 23:01:41 +0600

    CraftyBay ecommerce app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
+import 'package:crafty_bay/app.dart';
+import 'package:flutter/material.dart';
 
+/// layer First (data, ui, Domain layer)
+/// Feature first (Login->(data, ui,) , Card, others)
+
+void main() {
+  runApp(const CraftyBay());
+}
