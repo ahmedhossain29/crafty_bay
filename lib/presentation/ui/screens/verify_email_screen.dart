f7cfd147b7638bad47e4bd253161250621889a5a
AHMED <hossainahmad76@gmail.com>
2023-12-31 23:50:55 +0600

    verify email screen

---
+++ lib/presentation/ui/screens/verify_email_screen.dart
@@ -0,0 +1,61 @@
+import 'package:crafty_bay/presentation/ui/screens/otp_screen.dart';
+import 'package:crafty_bay/presentation/ui/widgets/app_logo.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 180,
+              ),
+              const AppLogo(
+                height: 100,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                'Welcome Back',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                'Please enter your email address',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.to(OtpScreen());
+                  },
+                  child: const Text('Next'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
