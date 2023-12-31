29ed6cedddafa6cf17a2d65c5456a651f9621788
AHMED <hossainahmad76@gmail.com>
2023-12-31 23:50:35 +0600

    Splash screen

--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:crafty_bay/presentation/ui/screens/verify_email_screen.dart';
+import 'package:crafty_bay/presentation/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,10 +12,37 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    moveToNextScreen();
+  }
+
+  void moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.offAll(const VerifyEmailScreen());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Column(
+          children: [
+            Spacer(),
+            AppLogo(
+              height: 120,
+            ),
+            Spacer(),
+            CircularProgressIndicator(),
+            SizedBox(
+              height: 16,
+            ),
+            Text('Version 10.'),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
