f4c90e4d8f97132f427ef67cddc9fdc9a3e848af
AHMED <hossainahmad76@gmail.com>
2024-01-05 21:29:45 +0600

    Nav bar

--- lib/presentation/ui/screens/main_bottom_nav_screen.dart
+++ lib/presentation/ui/screens/main_bottom_nav_screen.dart
@@ -10,6 +10,6 @@ class MainBottomNavScreen extends StatefulWidget {
 class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
