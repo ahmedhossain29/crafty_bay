80767e03e37432cd044716a8713131110e22ff42
AHMED <hossainahmad76@gmail.com>
2024-01-19 23:52:49 +0600

    Product details Screen

---
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -0,0 +1,234 @@
+import 'package:crafty_bay/presentation/ui/utility/app_colors.dart';
+import 'package:crafty_bay/presentation/ui/widgets/product_details/color_selector.dart';
+import 'package:crafty_bay/presentation/ui/widgets/product_details/product_image_carousel.dart';
+import 'package:crafty_bay/presentation/ui/widgets/product_details/size_selector.dart';
+import 'package:flutter/material.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
+
+class ProductDetailsScreen extends StatefulWidget {
+  const ProductDetailsScreen({super.key});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  ValueNotifier<int> noOfItems = ValueNotifier(1);
+  List<Color> colors = [
+    Colors.black,
+    Colors.amber,
+    Colors.grey,
+    Colors.green,
+    Colors.yellowAccent,
+  ];
+
+  List<String> sized = [
+    'S',
+    'L',
+    'M',
+    'XL',
+    'XXL',
+  ];
+
+  Color _selectedColor = Colors.black;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Details'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const ProductImageCarousel(),
+                  productDetailsBody,
+                ],
+              ),
+            ),
+          ),
+          priceAndAddToCartSection,
+        ],
+      ),
+    );
+  }
+
+  Padding get productDetailsBody {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Expanded(
+                child: Text(
+                  'Micropak MH 2024 Edition MMH652 -Save 30%',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+              ),
+              ValueListenableBuilder(
+                  valueListenable: noOfItems,
+                  builder: (context, value, _) {
+                    return ItemCount(
+                      initialValue: value,
+                      minValue: 0,
+                      maxValue: 20,
+                      decimalPlaces: 0,
+                      step: 1,
+                      color: AppColors.primaryColor,
+                      onChanged: (v) {
+                        setState(() {});
+                        noOfItems.value = v.toInt();
+                      },
+                    );
+                  }),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          reviewAndRatingRow,
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Color',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ColorSelector(
+              colors: colors,
+              onChange: (selectedColor) {
+                _selectedColor = selectedColor;
+              }),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Size',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          SizeSelector(size: sized, onChange: (s) {}),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Description',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          const Text(
+            '''Welcome contributions to Item Count Number Button! If you encounter any issues, have suggestions for improvements, or would like to add new features, please feel free to open an issue or submit a pull request. Your contributions can help make this package even better.''',
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row get reviewAndRatingRow {
+    return Row(
+      children: [
+        const Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            Icon(
+              Icons.star,
+              size: 18,
+              color: Colors.amber,
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Text(
+              '4.5',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black45,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        const Text(
+          'Review',
+          style: TextStyle(
+            fontSize: 16,
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Card(
+          color: AppColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Icon(
+              Icons.favorite_outline,
+              size: 18,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Container get priceAndAddToCartSection {
+  return Container(
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: AppColors.primaryColor.withOpacity(0.15),
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(16),
+        topRight: Radius.circular(16),
+      ),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Price',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black45),
+            ),
+            Text(
+              '\$1254',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primaryColor),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Add To Cart'),
+          ),
+        ),
+      ],
+    ),
+  );
+}
