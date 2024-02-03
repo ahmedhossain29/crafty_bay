6f34fb2204f14e0a4ce0a868922e792015dbbce6
AHMED <hossainahmad76@gmail.com>
2024-02-03 22:42:56 +0600

    add to cart controller

---
+++ lib/presentation/state_holders/add_to_cart_controller.dart
@@ -0,0 +1,28 @@
+import 'package:crafty_bay/data/services/network_caller.dart';
+import 'package:crafty_bay/data/utility/urls.dart';
+import 'package:get/get.dart';
+
+class AddToCartController extends GetxController {
+  bool _inProgress = false;
+
+  String _errorMessage = '';
+
+  bool get inProgress => _inProgress;
+
+  String get errorMessage => _errorMessage;
+
+  Future<bool> addToCart(
+      int productId, String color, String size, int qty) async {
+    bool isSuccess = false;
+    _inProgress = true;
+    Map<String, dynamic> inputParams = {
+      "product_id": productId,
+      "color": color,
+      "size": size,
+      "qty": qty
+    };
+    final response =
+        await NetworkCaller().postRequest(Urls.addToCart, body: inputParams);
+    if (response.isSuccess) {}
+  }
+}
