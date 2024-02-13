a58e91eb5c44ec723e7342c237b4f3f9f641eb9a
AHMED <hossainahmad76@gmail.com>
2024-02-13 23:39:23 +0600

    cart list controller

--- lib/presentation/state_holders/cart_list_controller.dart
+++ lib/presentation/state_holders/cart_list_controller.dart
@@ -1,5 +1,6 @@
 import 'package:crafty_bay/data/services/network_caller.dart';
 import 'package:crafty_bay/data/utility/urls.dart';
+import 'package:crafty_bay/presentation/state_holders/cart_item.dart';
 import 'package:get/get.dart';
 
 import 'cart_list_model.dart';
@@ -17,11 +18,15 @@ class CartListController extends GetxController {
 
   String get errorMessage => _errorMessage;
 
+  final RxDouble _totalPrice = 0.0.obs;
+
+  RxDouble get totalPrice => _totalPrice;
+
   Future<bool> getCartList() async {
     bool isSuccess = false;
     _inProgress = true;
     update();
-    final response = await NetworkCaller().postRequest(Urls.cartList);
+    final response = await NetworkCaller().getRequest(Urls.cartList);
     if (response.isSuccess) {
       _cartListModel = CartListModel.fromJson(response.responseData);
       isSuccess = true;
@@ -31,5 +36,21 @@ class CartListController extends GetxController {
     _inProgress = false;
     update();
     return isSuccess;
+  }
+
+  void updateQuantity(int id, int quantity) {
+    _cartListModel.cartItemList
+        ?.firstWhere((element) => element.id == id)
+        .quantity = quantity;
+    _totalPrice.value = _calculateTotalPrice;
+  }
+
+  double get _calculateTotalPrice {
+    double total = 0;
+    for (CartItem item in _cartListModel.cartItemList ?? []) {
+      total +=
+          (double.tryParse(item.product?.price ?? '0') ?? 0) * item.quantity;
+    }
+    return total;
   }
 }
