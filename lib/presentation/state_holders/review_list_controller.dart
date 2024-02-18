63c79cb3de534f2281f13389ef9531ea40c9f8c0
AHMED <hossainahmad76@gmail.com>
2024-02-18 23:55:57 +0600

    review list controller

---
+++ lib/presentation/state_holders/review_list_controller.dart
@@ -0,0 +1,34 @@
+import 'package:crafty_bay/data/models/review_list_model.dart';
+import 'package:crafty_bay/data/services/network_caller.dart';
+import 'package:crafty_bay/data/utility/urls.dart';
+import 'package:get/get.dart';
+
+class ReviewListController extends GetxController {
+  bool _inProgress = false;
+
+  String _errorMessage = '';
+
+  final ReviewListModel _reviewListModel = ReviewListModel();
+
+  ReviewListModel get reviewListModel => _reviewListModel;
+
+  bool get inProgress => _inProgress;
+
+  String get errorMessage => _errorMessage;
+
+  Future<bool> getReviewList(int productId) async {
+    bool isSuccess = false;
+    _inProgress = true;
+    update();
+    final response =
+        await NetworkCaller().getRequest(Urls.reviewList(productId));
+    if (response.isSuccess) {
+      isSuccess = true;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+    _inProgress = false;
+    update();
+    return isSuccess;
+  }
+}
