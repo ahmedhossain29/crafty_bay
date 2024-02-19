be96219e2447c9b68cdb3ded288c97eea95ed071
AHMED <hossainahmad76@gmail.com>
2024-02-19 22:48:59 +0600

    create product review screen

--- lib/presentation/ui/screens/create_review_screen.dart
+++ lib/presentation/ui/screens/create_review_screen.dart
@@ -1,14 +1,28 @@
+import 'package:crafty_bay/presentation/state_holders/create_product_review_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CreateReviewScreen extends StatelessWidget {
+class CreateReviewScreen extends StatefulWidget {
   const CreateReviewScreen({super.key});
+
+  @override
+  State<CreateReviewScreen> createState() => _CreateReviewScreenState();
+}
+
+class _CreateReviewScreenState extends State<CreateReviewScreen> {
+  final TextEditingController _reviewTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final CreateProductReviewController _createProductReviewController =
+      Get.find<CreateProductReviewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Get.back();
+          },
           icon: const Icon(Icons.arrow_back),
         ),
         title: Text(
@@ -18,26 +32,47 @@ class CreateReviewScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 52,
-            ),
-            TextFormField(
-              maxLines: 5,
-              decoration: const InputDecoration(hintText: 'Write Review'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Submit'),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 52,
               ),
-            ),
-          ],
+              TextFormField(
+                controller: _reviewTEController,
+                maxLines: 5,
+                decoration: const InputDecoration(hintText: 'Write Review'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _createProductReviewController.createReview(
+                          _reviewTEController.text.trim(),
+                          _createProductReviewController
+                              .productReviewModel.productReview!.id!
+                              .toInt(),
+                          _createProductReviewController
+                              .productReviewModel.productReview!.rating!
+                              .toInt(),
+                          _createProductReviewController
+                              .productReviewModel.productReview!.customerId!
+                              .toInt(),
+                          _createProductReviewController
+                              .productReviewModel.productReview!.productId!
+                              .toInt());
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
